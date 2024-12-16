9cc0c106c19962e0b99b49ad2497f94bedb71a20
blacksnow0 <86392451+blacksnow0@users.noreply.github.com>
2024-12-16 12:22:29 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRouter = require("./routes/userRoute");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5002;
 
